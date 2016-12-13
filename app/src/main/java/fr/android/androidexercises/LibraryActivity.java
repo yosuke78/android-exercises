dedb3e89cd9f4441d26bed88cf87228ad8aed974
yosuke78 <vannasay.souvannavong@gmail.com>
2016-12-13 16:58:43 +0100

    img

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -11,6 +14,13 @@ public class LibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_library);
 
         // TODO Get image view and load image form URL
+
+        ImageView downloadedImageView = (ImageView) findViewById(R.id.downloadImageView);
+        Picasso.with(this)
+                .load("http://img11.deviantart.net/555a/i/2016/217/2/c/goupix_vulpix_ice_form_alola_pokemon_moon_and_sun_by_naikodraw-dacs8dh.png")
+                .resize(200,200)
+                .centerCrop()
+                .into(downloadedImageView);
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
     }
 
